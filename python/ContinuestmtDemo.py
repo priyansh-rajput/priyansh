eb3aa98aabb2674d1e207c11c6c5fc2cb91c9923
priyansh-rajput <priyanshrajput38@yahoo.com>
2014-08-14 18:50:33 +0530

    All Pythom Demo Programmes

---
+++ python/ContinuestmtDemo.py
@@ -0,0 +1,12 @@
+str1="priyansh"
+for temp in str1:
+    print "Latter::",temp
+    if(temp=='a'):
+        continue
+a=10
+while(a>0):
+    print "Current Value Of A::",a
+    a=a-1;
+    if(a==5):
+        continue
+    
